908518d426de1c30eed1db134539bb252fc7a152
Simone Esposito <simoexpo@outlook.com>
2017-10-01 23:11:33 +0200

    added some tests

--- test/safe_ex_struct_test.exs
+++ test/safe_ex_struct_test.exs
@@ -7,20 +7,42 @@ defmodule SafeExStructTest do
     require SafeExStruct
 
     @safe_struct %{
-      name: :binary,
-      age: :integer
+      string: :binary,
+      num: :integer
     }
 
     SafeExStruct.create_struct
   end
 
-  defmodule ActualStruct do
+  defmodule NumberStruct do
     @behaviour SafeExStruct
     require SafeExStruct
 
     @safe_struct %{
-      name: :binary,
-      age: :number,
+      num: :number
+    }
+
+    SafeExStruct.create_struct
+  end
+
+  defmodule BitstringStruct do
+    @behaviour SafeExStruct
+    require SafeExStruct
+
+    @safe_struct %{
+      string: :bitstring
+    }
+
+    SafeExStruct.create_struct
+  end
+
+  defmodule ComplexStruct do
+    @behaviour SafeExStruct
+    require SafeExStruct
+
+    @safe_struct %{
+      string: :binary,
+      num: :number,
       other: SimpleStruct
     }
 
@@ -29,23 +51,41 @@ defmodule SafeExStructTest do
 
   test "SafeExStruct.create_struct add to a module a struct definition based on @safe_struct map" do
     struct = %SimpleStruct{}
-    assert Map.has_key?(struct, :name)
-    assert Map.has_key?(struct, :age)
+    assert Map.has_key?(struct, :string)
+    assert Map.has_key?(struct, :num)
     assert Map.has_key?(struct, :invalid_key) == false
   end
 
   test "SafeExStruct.create_struct add to a module a function is_valid that can validate the defined struct" do
-    good_struct = %SimpleStruct{name: "name", age: 18 }
+    good_struct = %SimpleStruct{string: "name", num: 18 }
     bad_struct = %SimpleStruct{}
     assert SimpleStruct.is_valid(good_struct)
     assert SimpleStruct.is_valid(bad_struct) == false
   end
 
   test "SafeExStruct.create_struct add to a module a function create_struct that create a struct from a map only if it's valid" do
-    good_map = %{name: "name", age: 18}
+    good_map = %{string: "name", num: 18}
     bad_map = %{}
-    assert SimpleStruct.create_struct(good_map) == {:ok, %SimpleStruct{name: "name", age: 18}}
+    assert SimpleStruct.create_struct(good_map) == {:ok, %SimpleStruct{string: "name", num: 18}}
     assert SimpleStruct.create_struct(bad_map) == {:error, :invalid_args}
+  end
+
+  test "is_valid should know that integer and float are number" do
+    assert NumberStruct.is_valid(%NumberStruct{num: 1})
+    assert NumberStruct.is_valid(%NumberStruct{num: 1.2})
+  end
+
+  test "is_valid should know that a string is a bitstring" do
+    assert(BitstringStruct.is_valid(%BitstringStruct{string: "ciao"}))
+  end
+
+  test "is_valid should work with nested struct" do
+    good_simple_struct = %SimpleStruct{string: "name", num: 18 }
+    bad_simple_struct = %SimpleStruct{}
+    good_complex_struct = %ComplexStruct{string: "name", num: 1, other: good_simple_struct}
+    bad_complex_struct = %ComplexStruct{string: "name", num: 1, other: bad_simple_struct}
+    assert ComplexStruct.is_valid(good_complex_struct)
+    assert ComplexStruct.is_valid(bad_complex_struct) == false
   end
 
 end
